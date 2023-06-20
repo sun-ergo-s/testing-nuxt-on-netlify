4f31b5e6b9d74ddb41a0cd94daad7b3dade27b7d
Michal <mvesely26@gmail.com>
2023-06-20 12:45:50 +0200

    ...

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -13,7 +13,7 @@ export default NuxtAuthHandler({
     ],
     cookies: {
         pkceCodeVerifier: {
-            name: "__Host-next-auth.pkce.code_verifier",
+            name: "__Secure-next-auth.pkce.code_verifier",
                 options: {
                 httpOnly: true,
                 sameSite: "none",
