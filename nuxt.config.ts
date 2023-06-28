a2fb149965e9e887de271c05d1be0ce71869f4b6
Michal <mvesely26@gmail.com>
2023-06-28 09:31:02 +0200

    ..

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,9 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   runtimeConfig: {
-    public: {
-        nuxtSecret: process.env.NUXT_SECRET,
-        googleClientId: process.env.GOOGLE_CLIENT_ID,
-        googleSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }
+      nuxtSecret: process.env.NUXT_SECRET,
+      googleClientId: process.env.GOOGLE_CLIENT_ID,
+      googleSecret: process.env.GOOGLE_CLIENT_SECRET,
   },
   modules: [
     '@sidebase/nuxt-auth'
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,11 +4,11 @@ import GoogleProvider from "next-auth/providers/google";
 const config = useRuntimeConfig()
 
 export default NuxtAuthHandler({
-    secret: config.public.nuxtSecret,
+    secret: config.nuxtSecret,
     providers: [
         GoogleProvider.default({
-            clientId: config.public.googleClientId,
-            clientSecret: config.public.googleSecret
+            clientId: config.googleClientId,
+            clientSecret: config.googleSecret
         })
     ],
     cookies: {
